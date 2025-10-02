43da6822bff8a4fee400b5dc3a5cfe45b4292b52
Pierre Ronceray <pierre.ronceray@univ-amu.fr>
2025-10-02 20:40:37 +0200

    Update __init__.py

--- SFI/__init__.py
+++ SFI/__init__.py
@@ -18,9 +18,16 @@ __all__ = [
     "OLI_bases", "OLI_inference", "SFI_data", "SFI_Langevin", "SFI_plotting_toolkit", "SFI_sparsity", "SFI_utils", "ULI_bases", "ULI_inference"
 ]
 
-__version__ = _v("SFI")
-del _v
+def _resolve_version():
+    for dist in ("stochasticforceinference", "SFI"):
+        try:
+            return _v(dist)
+        except PackageNotFoundError:
+            continue
+    return "0+unknown"
 
-"""
-"""
+__version__ = _resolve_version()
+del _v, PackageNotFoundError, _resolve_version
+
+
 
